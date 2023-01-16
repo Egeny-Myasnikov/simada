a2bdff62635fc97d7467fa793968bf411ef7c3cb
Egeny-Myasnikov <jenyamyasnikov@gmail.com>
2023-01-16 22:07:00 +0800

    create modalWindow

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { nav, openMenu } from "./parts/nav.js";
 import { phoneInputMask } from "./parts/phoneMask.js";
+import { modalWindow } from "./parts/modal-window.js";
 openMenu();
 nav();
 phoneInputMask();
+modalWindow(".wrapper");
 
 // console.log(1);
---
+++ js/parts/modal-window.js
@@ -0,0 +1,67 @@
+import { phoneInputMask } from "./phoneMask.js";
+
+export const modalWindow = (selector) => {
+  const leyoutModal = ({ title, text }) => `
+                      <div class="modal-window modal">
+                        <div class="modal__container">
+                            <span class="modal__close"></span>
+                            <h2 class="modal__title">${title}</h2>
+                            <div class="modal__body">
+                                <p class="modal__text">${text}</p>
+                                <form action="" class="modal__form">
+                                    <input class="modal__field" type="tel" data-input-phone
+                                        placeholder="+7 (999) 999-99-99" aria-placeholder="+79999999999" name="phone">
+                                    <button class="btn btn__solid btn__solid--black" type="submit">Отправить</button>
+                                    <button class="btn btn__transp btn__transp--black" type="reset">Отчистить</button>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="modal__background"></div>
+                      </div>
+  `;
+
+  const leyoutModalSelf = (self) => {
+    switch (self.dataset.modal) {
+      case "make":
+        return leyoutModal({
+          title: "Записаться",
+          text: "Мы свяжемся с вами в течение нескольких минут и&nbsp;запишем вас на удобную дату",
+        });
+      case "discount":
+        return leyoutModal({
+          title: "Получить скидку 20% на первый визит",
+          text: "Мы свяжемся с вами в течение нескольких минут и&nbsp;расскажем, как получить скидку",
+        });
+      case "buy":
+        return leyoutModal({
+          title: "Купить",
+          text: "Мы свяжемся с вами в течение нескольких минут и&nbsp;расскажем, как купить сетификат",
+        });
+      default:
+        return leyoutModal();
+    }
+  };
+
+  const closeModal = (btn, modal) => {
+    btn.addEventListener("click", () => {
+      modal.remove();
+    });
+  };
+  const showModal = (self) => {
+    const layout = leyoutModalSelf(self);
+    const parent = document.querySelector(selector);
+    parent.insertAdjacentHTML("afterBegin", layout);
+    phoneInputMask();
+  };
+
+  const btns = document.querySelectorAll(".btn");
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const self = e.currentTarget;
+      showModal(self);
+      const closeBtn = document.querySelector(".modal__close");
+      const modal = document.querySelector(".modal");
+      closeModal(closeBtn, modal);
+    });
+  });
+};
